a059713f9250dc07263719ba030b7459deb3ba6c
Akif Rabbani <akif@rabbani.my>
2016-09-23 13:43:51 +0800

    Added kiosk page

--- app/Http/Controllers/KioskController.php
+++ app/Http/Controllers/KioskController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Activity;
 use App\Http\Requests;
 
 class KioskController extends Controller
 {
     public function index() {
-    	return view('kiosk.index');
+    	return view('kiosk.index')->with(['list' => Activity::get()]);
     }
 
     
