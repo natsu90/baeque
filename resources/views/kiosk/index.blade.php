d7ad07d37c6211690f360441eb945e0703442c43
Akif Rabbani <akif@rabbani.my>
2016-09-23 13:45:07 +0800

    Added jquery

--- resources/views/kiosk/index.blade.php
+++ resources/views/kiosk/index.blade.php
@@ -106,7 +106,7 @@
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 </body>
 
